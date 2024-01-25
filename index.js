5854f59bb23d68229ce09f919005edd6b556e781
pallabofficial <officialpallabmondal000@gmail.com>
2024-01-25 16:50:00 +0530

    modified the timing function of the moving circle with the mouse pointer

--- index.js
+++ index.js
@@ -1,3 +1,28 @@
+// ---------------------------------------moving the green cursor and blurred cursor--------------------------------------
+function movingPointer(){
+    window.addEventListener("mousemove", function(event){
+        console.log(event);
+    })
+}
+movingPointer();
+
+
+
+let cursor = document.querySelector("#cursor");
+let cursorBlur = document.querySelector("#cursor-blur");
+
+document.addEventListener("mousemove", function(dets){
+    cursor.style.transform = `translate(${dets.clientX}px, ${dets.clientY}px)`;
+    cursorBlur.style.transform = `translate(${dets.clientX - 100}px , ${dets.clientY - 100}px)`;
+
+    // cursor.style.left = dets.x + "px";
+    // cursor.style.top = dets.y+ "px";
+    // cursorBlur.style.left = dets.x -100 + "px";
+    // cursorBlur.style.top = dets.y+ -100 + "px";
+})
+
+// ------------------------------------------navbar animations--------------------------------------
+
 gsap.to("#nav", {
     backgroundColor: "black",
     duration:"1",
@@ -5,7 +30,7 @@ gsap.to("#nav", {
     scrollTrigger:{
         trigger:"#nav",
         scroller:"body",
-        markers:true,
+        // markers:true,
         start:"top -10%",
         end:"top -11%",
         scrub:1,
@@ -13,12 +38,14 @@ gsap.to("#nav", {
     
 })
 
+// -------------------------------------------main div animations---------------------------------
+
 gsap.to("#main", {
     backgroundColor:"black", 
     scrollTrigger:{
         trigger:"#main",
         scroller:"body",
-        markers:true,
+        // markers:true,
         start:"top -25%",
         end:"top -70%",
         scrub:"3",
